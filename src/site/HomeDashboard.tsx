d296e6dc694913bf6595525e62da36cff5825909
ivanagbarreto <ivanagbarreto@gmail.com>
2025-06-09 15:28:33 -0300

    estilos bar button

--- src/site/HomeDashboard.tsx
+++ src/site/HomeDashboard.tsx
@@ -8,8 +8,11 @@ export const HomeDashboard = () =>{
 
         <header className="h-14 bg-blue-950 flex flex-row justify-between text-white"> 
             <div className="w-2/12 flex flex-row justify-center items-center">
-                <button className="bg-transparent hover:bg-blue-500 font-semibold">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={30}><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
+                <button className="bg-transparent hover:bg-blue-500 font-semibold
+                hover:text-white py-2 px-4 border
+                border-white hover:border-transparent rounded active:scale-95
+                ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width={30}><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
                 </button>
             </div>
             <span className="flex justify-center items-center w-10/12 bg-blue-600"></span>
